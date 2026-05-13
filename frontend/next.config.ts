d03722851df68f46cc68b837e8b61c44bc045208
Diego Passos <diego.spassos@souunit.com.br>
2026-05-13 15:46:11 -0300

    chore: update configs and dependencies
    - gitattributes for line endings
    - next.config with allowed dev origins
    - Updated dependencies

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,10 +4,16 @@ const nextConfig: NextConfig = {
   devIndicators: {
     position: "bottom-right",
   },
-  allowedDevOrigins: [
-    '192.168.18.70',
-    '192.168.56.1',
-  ],
+  allowedDevOrigins: ["192.168.18.70", "192.168.56.1"],
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://192.168.18.70:3001/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
